bc0c068c1ae4ec10cb5fe94c3bbf7b74e1afab8b
Tom <Tom@192.168.0.248>
2018-02-25 16:21:37 +0100

    ssdqq

--- js/mycode.js
+++ js/mycode.js
@@ -1,15 +1,10 @@
-<script>
-    // self, and window are both aliases for the host/window. 
-    // parent & top will also point to the window, except when lower down in a frame hierarchy
-    // if (top.location != document.location) top.location = document.location;
-    if(top!= self) top.location.href = self.location.href
-</script>
-
-
-        <script>
+window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')
+// self, and window are both aliases for the host/window. 
+// parent & top will also point to the window, except when lower down in a frame hierarchy
+// if (top.location != document.location) top.location = document.location;
+if(top!= self) top.location.href = self.location.href
 $(function(){
   $("#mynavbar").load("mynavbar.html"); 
   $("#mycarousel").load("mycarousel.html"); 
   $("#myfootertje").load("myfooter.html"); 
 });
-</script>
---
+++ js/mystrava.js
@@ -0,0 +1,53 @@
+hello.init({
+
+	strava: {
+
+		name: 'strava',
+
+		oauth: {
+			version: 2,    
+            auth: 'https://www.strava.com/oauth/authorize ',
+            grant: 'https://www.strava.com/oauth/token'
+			//auth: 'https://app.box.com/api/oauth2/authorize', // guess first leg (get using clientid)
+            //grant: 'https://api.box.com/oauth2/token', // this is for second leg (using POST ), client id & secret are passed
+			response_type: 'code'
+		},
+
+		base: 'https://www.strava.com/api/v3/',
+
+		get: {
+			me: 'users/me',
+			'me/files': 'files'
+		},
+
+		wrap: {
+			me: function(o) {
+				if (o.id) {
+					o.picture = o.thumbnail = o.avatar_url;
+					if (o.login.match('@')) {
+						o.email = o.login;
+					}
+				}
+
+				return o;
+			},
+
+			'me/files': function(o) {
+				if (Array.isArray(o)) {
+					return {data:o};
+				}
+
+				return o;
+			}
+		},
+
+		xhr: function(p) {
+
+			p.proxy = true;
+			p.proxy_response_type = 'proxy';
+			return true;
+		},
+
+		jsonp: false
+	}
+});
