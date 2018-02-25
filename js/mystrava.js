f88a620242cad46632c417bc2555e580d3465b80
Tom <Tom@192.168.0.248>
2018-02-25 23:26:30 +0100

    dcds

--- js/mystrava.js
+++ js/mystrava.js
@@ -15,9 +15,12 @@ hello.init({
              http://www.strava.com/oauth/authorize?client_id=23719&response_type=code&redirect_uri=https://tommyclemansi.github.io/abloc/members.html&state=%7B"client_id"%3A"23719"%2C"network"%3A"strava"%2C"display"%3A"popup"%2C"callback"%3A"_hellojs
              
              https://github.com/MrSwitch/hello.js/issues/392
+             
+             
+             https://www.strava.com/oauth/authorize?approval_prompt=force&client_id=23719&response_type=code&redirect_uri=https%3A%2F%2Ftommyclemansi.github.io%2Fabloc%2Fmembers.html&state=%7B%22client_id%22%3A%2223719%22%2C%22network%22%3A%22strava%22%2C%22display%22%3A%22popup%22%2C%22callback%22%3A%22_hellojs_4u17cdgp%22%2C%22state%22%3A%22%22%2C%22redirect_uri%22%3A%22https%3A%2F%2Ftommyclemansi.github.io%2Fabloc%2Fmembers.html%22%2C%22scope%22%3A%22basic%22%2C%22oauth%22%3A%7B%22version%22%3A2%2C%22auth%22%3A%22http%3A%2F%2Fwww.strava.com%2Foauth%2Fauthorize%3Fapproval_prompt%3Dforce%22%2C%22grant%22%3A%22http%3A%2F%2Fwww.strava.com%2Foauth%2Ftoken%22%2C%22response_type%22%3A%22code%22%7D%2C%22oauth_proxy%22%3A%22https%3A%2F%2Fauth-server.herokuapp.com%2Fproxy%22%7D&scope=
             */
-            auth: 'http://www.strava.com/oauth/authorize?approval_prompt=force',
-            grant: 'http://www.strava.com/oauth/token',
+            auth: 'https://www.strava.com/oauth/authorize?approval_prompt=force',
+            grant: 'https://www.strava.com/oauth/token',
 			//auth: 'https://app.box.com/api/oauth2/authorize', // guess first leg (get using clientid)
             //grant: 'https://api.box.com/oauth2/token', // this is for second leg (using POST ), client id & secret are passed
 			response_type: 'code'
