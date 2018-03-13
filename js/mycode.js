97f1f1f9eccddaa480be13bc8d4aca2b75fa9bf6
Tom <Tom@192.168.0.248>
2018-03-13 22:18:46 +0100

    fqdsfds

--- js/mycode.js
+++ js/mycode.js
@@ -1,7 +1,7 @@
 function enableNoResize() 
 {
-    //var frame = window.document.getElementsByName("dot_tk_frame_content")[0]
-    var myframe = document.getElementsByTagName('frame')[0];
+    var frame = window.parent.document.getElementsByName("dot_tk_frame_content")[0]
+    //var myframe = document.getElementsByTagName('frame')[0];
     //var frame = parent.frames['frame1'] || parent.document.getElementById('dot_tk_frame_content');
     if (myframe != null)
     {myframe.noResize = "false";
