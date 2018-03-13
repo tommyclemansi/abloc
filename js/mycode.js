23ce641bd6bf494a58dca1e4e996f1a040b1f4c5
Tom <Tom@192.168.0.248>
2018-03-13 21:47:47 +0100

    wccxwcx

--- js/mycode.js
+++ js/mycode.js
@@ -3,7 +3,8 @@ function enableNoResize()
     var frame = window.document.getElementsByName(dot_tk_frame_content)[0]
     //var frame = parent.frames['frame1'] || parent.document.getElementById('dot_tk_frame_content');
     if (frame != null)
-    {frame.noResize = "true";
-    frame.setAttribute('noResize', true);
+    {frame.noResize = "false";
+    frame.setAttribute('noResize', false);
+     console.log(frame noresize attribute set to false);
     }
 }
