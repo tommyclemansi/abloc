f5ba74cbcfdf973f7b7ec39b0aa0addee3442562
Tom <Tom@192.168.0.248>
2018-03-13 21:52:13 +0100

    fix

--- js/mycode.js
+++ js/mycode.js
@@ -1,6 +1,6 @@
 function enableNoResize() 
 {
-    var frame = window.document.getElementsByName(dot_tk_frame_content)[0]
+    var frame = window.document.getElementsByName("dot_tk_frame_content")[0]
     //var frame = parent.frames['frame1'] || parent.document.getElementById('dot_tk_frame_content');
     if (frame != null)
     {frame.noResize = "false";
