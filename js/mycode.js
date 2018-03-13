9e231e99d11ba2de16acd216a3450335522b048e
Tom <Tom@192.168.0.248>
2018-03-13 22:12:35 +0100

    xcwcxw

--- js/mycode.js
+++ js/mycode.js
@@ -1,10 +1,11 @@
 function enableNoResize() 
 {
-    var frame = window.document.getElementsByName("dot_tk_frame_content")[0]
+    //var frame = window.document.getElementsByName("dot_tk_frame_content")[0]
+    var myframe = document.getElementsByTagName('frame')[0];
     //var frame = parent.frames['frame1'] || parent.document.getElementById('dot_tk_frame_content');
-    if (frame != null)
-    {frame.noResize = "false";
-    frame.setAttribute('noResize', false);
+    if (myframe != null)
+    {myframe.noResize = "false";
+    myframe.setAttribute('noResize', false);
      console.log("frame noresize attribute set to false");
     }
     else {
